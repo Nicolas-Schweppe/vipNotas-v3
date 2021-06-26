45e94629d04c1f20674d0d06abfd9e065b6a0023
schweppe nicolas <nicolas_vialidad@hotmail.com>
2021-06-26 16:47:46 -0300

    update

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const { use } = require('./routes/users.routes');
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} =process.env; //traigo variables de env
 const MONGODB_URI= `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
-
+const nueva='mongodb+srv://nicolas:FX8350amdr9280X_@cluster0.nzdq0.mongodb.net/notes-app?retryWrites=true&w=majority'
 
 mongoose.connect(MONGODB_URI,{
     useUnifiedTopology:true,
