a30f5858a1861f061642c5526b1bcc1684b54635
Nicolas-Schweppe <nicolas_vialidad@hotmail.com>
2021-09-29 20:41:36 -0300

    update

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ require('./config/passport');
 
 //setting
 
-app.set('port',process.env.PORT || 4000 ,  function(){
+app.set('port',process.env.PORT || 8080 ,  function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
